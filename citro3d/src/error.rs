d04f647068b7abbe1cd69aea02edf6c234a0065d
Natasha England-Elbro <natasha@natashaee.me>
2024-01-02 20:57:53 +0000

    fix: errors do not implement std::error::Error

--- citro3d/src/error.rs
+++ citro3d/src/error.rs
@@ -12,29 +12,39 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 /// The common error type that may be returned by `citro3d` functions.
 #[non_exhaustive]
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// C3D error code.
+    #[error("C3D error, code: {0}")]
     System(libc::c_int),
     /// A C3D object or context could not be initialized.
+    #[error("a C3D object or context failed to initialize")]
     FailedToInitialize,
     /// A size parameter was specified that cannot be converted to the proper type.
+    #[error("specified size parameter is invalid")]
     InvalidSize,
     /// Failed to select the given render target for drawing to.
+    #[error("render target is invalid")]
     InvalidRenderTarget,
     /// Indicates that a reference could not be obtained because a lock is already
     /// held on the requested object.
+    #[error("a lock is already held for requested object")]
     LockHeld,
     /// Indicates that too many vertex attributes were registered (max 12 supported).
+    #[error("too many vertex attribute requested (max of 12)")]
     TooManyAttributes,
     /// Indicates that too many vertex buffer objects were registered (max 12 supported).
+    #[error("too many vertex buffer objects registered (max of 12)")]
     TooManyBuffers,
     /// The given memory could not be converted to a physical address for sharing
     /// with the GPU. Data should be allocated with [`ctru::linear`].
+    #[error("invalid memory location, address should be allocated with `ctru::linear`")]
     InvalidMemoryLocation,
     /// The given name was not valid for the requested purpose.
+    #[error("provided name is invalid")]
     InvalidName,
     /// The requested resource could not be found.
+    #[error("requested resource could not be found")]
     NotFound,
 }
 
